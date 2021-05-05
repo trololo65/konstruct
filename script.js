823a908d30bd5c3e32d74c8f4ac30d4f3555ea80
trololo65 <82900214+trololo65@users.noreply.github.com>
2021-05-05 23:48:33 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+	$('.navbar_burger').click(function(event){
+		$('.navbar_burger,.navbar_link').toggleClass('active');
+		$('body').toggleClass('lock')
+	});
+})
+$(document).ready(function(){
+	$('.flowing-scroll').click(function(event){
+		$('body').toggleClass('lock');
+		$('header').toggleClass('delete')
+
+	});
+})
+
+$('.flowing-scroll').on('click', function() {
+  var el = $(this);
+  var dest = el.attr('href'); // получаем направление
+  if (dest !== undefined && dest !== '') { // проверяем существование
+    $('html').animate({
+        scrollTop: $(dest).offset().top // прокручиваем страницу к требуемому элементу
+      }, 500 // скорость прокрутки
+    );
+  }
+  return false;
+});
