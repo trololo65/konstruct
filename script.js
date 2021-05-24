bfdcec65f0d3456523b552a048696c3f337a688c
trololo65 <82900214+trololo65@users.noreply.github.com>
2021-05-24 16:10:53 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -4,13 +4,7 @@ $(document).ready(function(){
 		$('body').toggleClass('lock');
 	});
 })
-$(document).ready(function(){
-	$('.flowing-scroll').click(function(event){
-		
 
-
-	});
-})
 
 $('.flowing-scroll').on('click', function() {
   var el = $(this);
@@ -23,3 +17,62 @@ $('.flowing-scroll').on('click', function() {
   }
   return false;
 });
+$('.scroll-top').on('click', function() {
+  var el = $(this);
+  var dest = el.attr('href'); // получаем направление
+  if (dest !== undefined && dest !== '') { // проверяем существование
+    $('html').animate({
+        scrollTop: $(dest).offset().top // прокручиваем страницу к требуемому элементу
+      }, 500 // скорость прокрутки
+    );
+  }
+  return false;
+});
+
+/*
+$(window).scroll(function (event) {
+    var top = $(window).scrollTop();
+     if(top >= 20){
+	     //show btn
+	     $('.scroll-top').toggleClass('show_button');
+	     console.log('show_button')
+     } else {
+     	//hide btn
+     	 $('.scroll-top').toggleClass('hide_button');
+     	 console.log('hide_button')
+     
+     }
+});
+*/
+const animItems = document.querySelectorAll('.anim-items')
+
+if (animItems.length > 0) {
+	window.addEventListener('scroll', animOnScroll);
+	function animOnScroll(params) {
+		for (let i = 0; i < animItems.length ; i++) {
+			const animItem = animItems[i];
+			const animItemHeight = animItem.offsetHeight;
+			const animItemOffset = offset(animItem).top;
+			const animStart = 4;
+
+			let animItemPoint = window.innerHeight - animItemHeight / animStart;
+
+			if (animItemHeight > window.innerHeight) {
+				animItemPoint = window.innerHeight - window.innerHeight / animStart;
+			}
+			if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+				animItem.classList.add('active');
+			} else {
+				if (!animItem.classList.contains('anim-no-hide')) {
+					animItem.classList.remove('active');
+				}
+			}
+		}
+	}
+	function offset(el) {
+		const rect = el.getBoundingClientRect(),
+			scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+			scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+		return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+	}
+}
